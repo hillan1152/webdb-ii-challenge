e6a7c4c556c0296600e183d230c1b6a527e1c589
Anthony Hill <hillan1152@gmail.com>
2019-12-10 18:18:12 -0500

    MVP, set up GETS and POST

--- cars/carsRouter.js
+++ cars/carsRouter.js
@@ -3,28 +3,56 @@ const express = require('express');
 const knex = require('knex');
 
 // HOOK UP DATABASE USING KNEX
-const db = knex({
-    client: 'sqlite3',
-    connection: {
-        filename: './data/cars.db3'
-    },
-    useNullAsDefault: true
-});
+const config = require('../knexfile.js')
+const db = knex(config.development);
+// const db = knex({
+// 	client: 'sqlite3',
+// 	connection: {
+// 		filename: './data/cars.db3'
+// 	},
+// 	useNullAsDefault: true
+// });
 
 // HOOK UP ROUTER
 const router = express.Router();
 
+// GET CARS
+router.get('/', (req, res) => {
+	db('car-dealer')
+		.then((car) => {
+			res.status(200).json(car);
+		})
+		.catch((err) => {
+			res.status(500).json({ message: 'Failed to retrieve cars' });
+		});
+});
+router.get('/:id', (req, res) => {
+	const { id } = req.params;
+    db('car-dealer')
+        .where({ id }).first()
+		.then((car) => {
+			res.status(200).json(car);
+		})
+		.catch((err) => {
+			res.status(500).json({ message: 'Failed to retrieve cars' });
+		});
+});
 
-
+// POST A CAR
 router.post('/', (req, res) => {
-    const carData = req.body;
-})
+	const carData = req.body;
 
-
-
-
-
-
-
+	db('car-dealer')
+		.insert(carData)
+		.then((ids) => {
+			db('car-dealer').where({ id: ids[0] }).then((newCar) => {
+				res.status(201).json(newCar);
+			});
+		})
+		.catch((err) => {
+			console.log('POST ERROR', err);
+			res.status(500).json({ message: 'Failed to post data.' });
+		});
+});
 
 module.exports = router;
